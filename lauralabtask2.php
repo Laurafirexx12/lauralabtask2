a93eabde142115a3fee73a3467865385d045c7f1
Laurafirexx12 <lauramanhondo1@gmail.com>
2022-05-12 16:46:58 +0200

    exercise4

--- lauralabtask2.php
+++ lauralabtask2.php
@@ -4,3 +4,12 @@ function greetings(string $text): void
     echo "$text";
 }
 greetings('param3t3r');
+function repeatContent(int $count = 1):void 
+{
+    for($i=0;$i <=$count;$i++){
+        greetings("is school hard");
+
+    }
+}
+repeatContent();
+repeatContent(5);
