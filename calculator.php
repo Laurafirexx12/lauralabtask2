089a17908bba2ceda08c225f50ee44196a28ff9a
Laurafirexx12 <lauramanhondo1@gmail.com>
2022-05-13 12:56:47 +0200

    task6

--- calculator.php
+++ calculator.php
@@ -16,14 +16,19 @@ echo diff(10,2)."<br/>";
 echo div(10,2)."<br/>";
 echo mult(10,2)."<br/>";
 
-function calculator2(float $num1,string $opp, float $num2,):float{
-    if(!in_array($opp,['+','-','*','/'])){
-
+ function calculator(float $num1,string $opp,float $num2,): float{
+    if(!($opp =='+' or $opp == '-' or $opp == '/' or $opp == '*')){
+        return 0;
     }
-    $fucnt_name=['+'=>'sum',
-                 '-'=>'diff',
-                 '/'=>'div',
-                 '*'=>'mult'];
-    return $fucnt_name[$opp]($num1,$num2);                  
+    switch ($opp){
+        case '+': return sum($num1,$num2);
+        case'-': return diff($num1,$num2);
+        case '/':return div($num1,$num2);
+        case '*': return mult($num,$num2);
+        
+    }
 }
-echo calculator2(1,'+',3);
+echo calculator(1,'+',2)."<br/>";
+echo calculator(1,'-',2)."<br/>";
+echo calculator(1,'/',2)."<br/>";
+echo calculator(1,'*',2)."<br/>";
